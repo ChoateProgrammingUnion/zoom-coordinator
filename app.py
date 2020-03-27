1a573ea5ae72d3024a3a90592fc9926db1eb922c
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-03-27 09:19:53 -0400

    Added authentication and SSO

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+import os
+from flask_dance.contrib.google import make_google_blueprint, google 
 from flask_caching import Cache
+from config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 import time
 import secrets
 
-random_secret_key = secrets.token_urlsafe(128)
+# Temp (INSECURE, REMOVE IN PROD)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+app = Flask(__name__)
+random_secret_key = secrets.token_urlsafe(32)
 app.config.update(
     DEBUG=False,
     SECRET_KEY=random_secret_key
@@ -16,7 +23,54 @@ cache = Cache(app, config={
 })
 
 
+# Credit: oauth boilerplate stuff from library documentation
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = GOOGLE_CLIENT_ID 
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = GOOGLE_CLIENT_SECRET 
+app.config["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "true"
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "true" 
+
+google_bp = make_google_blueprint(scope=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"])
+app.register_blueprint(google_bp, url_prefix="/login")
+
+@google_bp.session.authorization_required
+@app.route('/restricted')
+def restricted():
+    return "Hello, world!"
+
 @app.route('/')
-@cache.cached()
 def index():
-    pass
+    # if email := get_email():
+    email = get_email()
+    if email:
+        return email
+    else:
+        return redirect("/login")
+
+@app.route('/login')
+def login():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+
+def get_email():
+    """
+    Checks and sanitizes email. 
+    Returns false if not logged in or not choate email.
+    """
+    try:
+        if google.authorized:
+            resp = google.get("/oauth2/v1/userinfo")
+            if resp.ok and resp.text:
+                response = resp.json()
+                email = str(response.get("email"))
+                if check_choate_email(email):
+                    return email
+    except:
+        pass
+    return False
+
+
+def check_choate_email(email: str) -> bool:
+    if email.endswith("@choate.edu") and email.count("@") == 1 and email.count(".") == 1:
+        return True
+    else:
+        return False
