6f6102088849aadbbd86134edb6c1f586fd0d1d4
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-03-27 09:21:31 -0400

    Added boilerplate schedule class/module

---
+++ schedule.py
@@ -0,0 +1,9 @@
+class Schedule:
+    """
+    Schedule will fetch the student's schedule and pass it back as a dict
+    """
+    def __init__(self):
+        pass
+    
+    def fetch_schedule(self):
+        pass
