ba11d1cb5382e4098d81c04b12886bb200760bdf
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-03-28 10:25:12 -0400

    Updated button and removed automatic reload

--- templates/card.js
+++ templates/card.js
@@ -1,9 +1,33 @@
+
+           function sendRequest{{ uuid }}(fd, loc) {
+                var xhr = new XMLHttpRequest();
+                xhr.onload = function(e) {
+                    if (this.readyState === 4) {
+                        console.log("Server returned: ", e.target.responseText);
+                        if (e.target.responseText == "Success!") {
+                            meetingId = document.getElementsByClassName("meeting_id{{ uuid }}")[0].value
+                            $( "#span{{ uuid }}" ).html("<button type=\"button\" class=\"btn btn-outline-info\" onclick=\"window.open('https://zoom.us/j/" + meetingId + "');\">Join Zoom Meeting (id: " + meetingId + ")</button>");
+                        }
+                    }
+                };
+                xhr.timeout = 5000;
+                xhr.open("POST", loc, true);
+                xhr.send(fd);
+                console.log(fd);
+                for (var value of fd.values()) {
+                   console.log("{{ uuid }}", value);
+                }
+                for (var value of fd.keys()) {
+                   console.log("{{ uuid }}", value);
+                }
+            }
+
             function submit{{ uuid }}() {
                 var fd = new FormData();
                 fd.append("meeting_id", document.getElementsByClassName("meeting_id{{ uuid }}")[0].value);
                 fd.append("course", "{{ course }}");
                 fd.append("section", "{{ sec }}");
-                sendRequest(fd, "update");
+                sendRequest{{ uuid }}(fd, "update");
             }
 
             $(window).on('load', function() {
@@ -12,6 +36,3 @@
                   console.log( "Handler for {{ uuid }} called." );
                 });
             });
-
-
-
