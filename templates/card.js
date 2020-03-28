c6d306667b7cbe8cd708cb906b10dcfaa3e7bd8d
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-03-27 23:05:24 -0400

    Added auto-refresh

--- templates/card.js
+++ templates/card.js
@@ -1,13 +1,13 @@
             function submit{{ uuid }}() {
                 var fd = new FormData();
-                fd.append("meeting-id", document.getElementsByClassName("meeting_id{{ uuid }}")[0].value);
+                fd.append("meeting_id", document.getElementsByClassName("meeting_id{{ uuid }}")[0].value);
                 fd.append("course", "{{ course }}");
                 fd.append("section", "{{ sec }}");
                 sendRequest(fd, "update");
             }
 
             $(window).on('load', function() {
-                $( "#meeting_id{{ uuid }}" ).keypress(function() {
+                $( "#meeting_id{{ uuid }}" ).keyup(function() {
                   submit{{ uuid }}();
                   console.log( "Handler for {{ uuid }} called." );
                 });
