94d97ec671be057bb55fe752801af9135d1e85cf
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-03-30 18:24:20 -0400

    Fixed namespace pollution error (potential)

--- ical.py
+++ ical.py
@@ -19,8 +19,8 @@ def make_calendar(email, name):
     cal = Calendar()
     cal['summary'] = 'Choate Zoom Coordinator Schedule'
     for count, each_day in enumerate(CLASSDAYS):
-        for block, time in block_iter(email, datetime_needed=True, weekday=each_day):
-            time = time.replace(day=this_monday.day) + datetime.timedelta(days=count)
+        for block, start_time in block_iter(email, datetime_needed=True, weekday=each_day):
+            start_time = start_time.replace(day=this_monday.day) + datetime.timedelta(days=count)
             
             block_data = user_schedule.schedule[block]
             if block_data:
@@ -31,8 +31,8 @@ def make_calendar(email, name):
                 event['LOCATION'] = "https://zoom.us/j/" + str(block_data.get('meeting_id'))
                 event['summary'] = block_data.get('course_name').title()
                 event['DESCRIPTION'] = "Zoom link: https://zoom.us/j/" + str(block_data.get('meeting_id')) + "\nMeeting ID: " + str(block_data.get('meeting_id'))
-                event.add('dtstart', time)
-                event.add('dtend', time + datetime.timedelta(minutes=50))
+                event.add('dtstart', start_time)
+                event.add('dtend', start_time + datetime.timedelta(minutes=50))
                 cal.add_component(event)
     return cal
 
